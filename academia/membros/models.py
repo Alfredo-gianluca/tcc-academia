575bd63881673c1937b3911e779093d64496b8eb
Alfredo-gianluca <alfredo.gian.salvarani@gmail.com>
2025-05-22 08:25:45 -0300

    Colocando peso
    Colocando o atributo "peso" na classe Membro do modelo

--- academia/membros/models.py
+++ academia/membros/models.py
@@ -7,6 +7,7 @@ class Membro(models.Model):
   data_nascimento = models.DateField(null=True, blank=True)
   telefone = models.CharField(max_length=20, blank=True)
   data_cadastro = models.DateTimeField(auto_now_add=True)
+  peso = models.FloatField(null=True, blank=True)
 
   def __str__(self):
     return f"{self.nome} {self.sobrenome}"

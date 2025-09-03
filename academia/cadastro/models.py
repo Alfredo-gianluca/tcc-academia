0508f4c699d6fe62d4b81609338c1d49cb6739a4
Alfredo-gianluca <alfredo.gian.salvarani@gmail.com>
2025-09-03 15:53:30 -0300

    Colocando bd
    Aplicando bd a tela de cadastro

--- academia/cadastro/models.py
+++ academia/cadastro/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class UsuarioC(models.Model):
+    GENERO_CHOICES = [
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+        ('O', 'Outro'),
+        ('N', 'Prefiro não dizer'),
+    ]
+
+    nome_completo = models.CharField(max_length=100)
+    senha = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    telefone = models.CharField(max_length=20)
+    data_nascimento = models.DateField()
+    genero = models.CharField(max_length=1, choices=GENERO_CHOICES)
+    observacoes = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nome_completo
--- academia/cadastro/views.py
+++ academia/cadastro/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from .forms import UsuarioCForm
 
 def cadastro(request):
+    if request.method == 'POST':
+        form = UsuarioCForm(request.POST)
+        if form.is_valid():
+            usuario.senha = make_password(form.cleaned_data['senha'])
+            usuario.save()
+            form.save()
+            return render(request, 'cadastroSucesso.html')
+    else:
+        form = UsuarioCForm()
     return render(request, 'cadastro.html', {
+        'form': form,
         'centralizar_logo': True
     })
 

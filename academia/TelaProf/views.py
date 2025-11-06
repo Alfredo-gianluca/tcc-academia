7f89ebf68cbc190db7f219fd65f1266ab972f055
Alfredo-gianluca <alfredo.gian.salvarani@gmail.com>
2025-11-06 12:09:53 -0300

    arrumando calendário na tela prof
    arrumando o calendário na tela prof, que agora salva as informações no CalendarioFrequencia e puxa as alterações antes feitas

--- academia/TelaProf/views.py
+++ academia/TelaProf/views.py
@@ -1,8 +1,9 @@
+import json
 from django.shortcuts import render, get_object_or_404, redirect
 from cadastro.models import Usuario, CalendarioFrequencia
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseForbidden
 from datetime import datetime
+
 
 @login_required
 def lista_alunos(request):
@@ -15,8 +16,9 @@ def editar_aluno(request, aluno_id):
     aluno = get_object_or_404(Usuario, id=aluno_id)
     calendario = CalendarioFrequencia.objects.filter(usuario=aluno).order_by('-data')
 
-    # --- Adicionar data de presença ---
     if request.method == 'POST':
+
+        # --- Adicionar nova data ---
         if 'adicionar_data' in request.POST:
             data_str = request.POST.get('nova_data')
             if data_str:
@@ -24,18 +26,31 @@ def editar_aluno(request, aluno_id):
                     data = datetime.strptime(data_str, '%Y-%m-%d').date()
                     CalendarioFrequencia.objects.get_or_create(usuario=aluno, data=data)
                 except ValueError:
-                    pass  # data inválida, ignorar ou tratar com mensagem
-            
-        # --- Remover data de presença ---
+                    pass  # Data inválida
+
+        # --- Remover data ---
         elif 'remover_data' in request.POST:
             data_id = request.POST.get('data_id')
             if data_id:
                 CalendarioFrequencia.objects.filter(id=data_id, usuario=aluno).delete()
 
-        # --- Atualizar observações ---
-        elif 'salvar_observacoes' in request.POST:
-            aluno.observacoes = request.POST.get('observacoes')
+        # --- Salvar observações e presenças ---
+        else:
+            aluno.observacoes = request.POST.get('observacoes', '')
             aluno.save()
+
+            presencas_json = request.POST.get('presencas_json', '{}')
+            try:
+                presencas_data = json.loads(presencas_json)
+                for freq_id, presente in presencas_data.items():
+                    try:
+                        freq = CalendarioFrequencia.objects.get(id=freq_id, usuario=aluno)
+                        freq.presente = bool(presente)
+                        freq.save()
+                    except CalendarioFrequencia.DoesNotExist:
+                        pass
+            except json.JSONDecodeError:
+                pass  # Ignora se o JSON vier malformado
 
         return redirect('TelaProf:editar_aluno', aluno_id=aluno.id)
 
@@ -43,4 +58,3 @@ def editar_aluno(request, aluno_id):
         'aluno': aluno,
         'calendario': calendario,
     })
-
